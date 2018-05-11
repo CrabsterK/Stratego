b3054c8d13fa2b948fffe186878b5189527fda9c
CrabsterK <arek.marcinowski18@gmail.com>
2018-05-11 17:45:34 +0200

    move move function to Player

--- src/main/java/com/si/Player.java
+++ src/main/java/com/si/Player.java
@@ -2,9 +2,11 @@ package com.si;
 
 public class Player {
     public int points;
+    public Board board;
 
-    public Player(){
-        points = 0;
+    public Player(Board board){
+        this.points = 0;
+        this.board = board;
     }
 
     public int getPoints(){
@@ -13,5 +15,18 @@ public class Player {
 
     public void addPoints(int pointsToAdd){
         this.points += pointsToAdd;
+    }
+
+    public void move(int x, int y){
+        int points = board.putAndGetNewPoints(x, y);
+        if(points == 0){
+            System.out.println("Ruch niepunktowany");
+        }
+        else if (points > 0){
+            addPoints(points);
+        }
+        if(points < 0){
+            System.out.println("Coś poszło nie tak, Menager.move(" + x + ", " + y + ")");
+        }
     }
 }

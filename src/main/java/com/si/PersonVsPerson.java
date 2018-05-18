f879ac963de7b2dc4980ef5aba5b047acaf3b9ba
CrabsterK <arek.marcinowski18@gmail.com>
2018-05-19 00:25:26 +0200

    add GUI in pvp, choice of gui, choice of first player, make run() shhorter

--- src/main/java/com/si/PersonVsPerson.java
+++ src/main/java/com/si/PersonVsPerson.java
@@ -4,33 +4,47 @@ import com.si.player.Player;
 
 import java.util.Scanner;
 
-public class PersonVsPerson {
+public class PersonVsPerson implements Game{
     private final int BOARD_SIZE;
     private Board board;
     private Player player1;
     private Player player2;
     private Scanner sc;
+    private boolean enableGui;
+    private boolean player1MoveFirst;
+    GameWindow g;
 
-    public PersonVsPerson(int n){
+    public PersonVsPerson(int n, boolean enableGui, boolean player1MoveFirst){
         BOARD_SIZE = n;
         board = new Board(BOARD_SIZE);
         player1 = new Player(board);
         player2 = new Player(board);
         sc = new Scanner(System.in);
+        this.enableGui = enableGui;
+        this.player1MoveFirst = player1MoveFirst;
+
     }
 
     public void run() {
         while (!board.isFull()) {
-            board.print();
-
             int errorCode;
             do {
-                System.out.println("Róch gracza I");
+                if(player1MoveFirst) {
+                    System.out.println("Ruch gracza I");
+                }
+                else {
+                    System.out.println("Ruch gracza II");
+                }
                 System.out.println("x: ");
-                int player1x = sc.nextInt();
+                int playerX = sc.nextInt();
                 System.out.println("y: ");
-                int player1y = sc.nextInt();
-                errorCode = player1.move(player1x, player1y);
+                int playerY = sc.nextInt();
+                if(player1MoveFirst) {
+                    errorCode = player1.move(playerX, playerY);
+                }
+                else {
+                    errorCode = player2.move(playerX, playerY);
+                }
 
                 if (errorCode == 2) {
                     System.out.println("Pole zajęte");
@@ -42,33 +56,38 @@ public class PersonVsPerson {
                 }
             }
             while (errorCode == 2 || errorCode == 4);
+
+            board.print();
             System.out.println("pkt1: " + player1.getPoints());
             System.out.println("pkt2: " + player2.getPoints());
-            board.print();
+            System.out.println();
+        }
+    }
 
 
-            int errorCode2;
-            do {
-                System.out.println("Róch gracza II");
-                System.out.println("x: ");
-                int player2x = sc.nextInt();
-                System.out.println("y: ");
-                int player2y = sc.nextInt();
-                errorCode2 = player2.move(player2x, player2y);
 
-                if (errorCode2 == 2) {
-                    System.out.println("Pole zajęte");
-                    System.out.println("Powtórz ruch");
-                }
-                if (errorCode2 == 4) {
-                    System.out.println("Wartość spoza zakresu");
-                    System.out.println("Powtórz ruch");
-                }
-            }
-            while (errorCode2 == 2 || errorCode2 == 4);
-            System.out.println("pkt1: " + player1.getPoints());
-            System.out.println("pkt2: " + player2.getPoints());
-            board.print();
+    public void run(int[] positions) {
+        if (player1MoveFirst){
+            player1.move(positions[0], positions[1]);
+        }
+        else {
+            player2.move(positions[0], positions[1]);
+        }
+        player1MoveFirst = !player1MoveFirst;
+        board.print();
+        System.out.println("pkt1: " + player1.getPoints());
+        System.out.println("pkt2: " + player2.getPoints());
+        System.out.println();
+    }
+
+    public void runGame() {
+        if (enableGui){
+            g = new GameWindow("Game", BOARD_SIZE, this);
+            g.setVisible(true);
+            g.setSize(500, 500);
+        }
+        else {
+            run();
         }
     }
 }

23b640ba59df62fe5fe4794b89c27a2782d2346f
CrabsterK <arek.marcinowski18@gmail.com>
2018-05-20 01:16:05 +0200

    delete Player, add Player abstract

--- src/main/java/com/si/player/Player.java
+++ src/main/java/com/si/player/Player.java
@@ -2,40 +2,7 @@ package com.si.player;
 
 import com.si.Board;
 
-public class Player implements PlayerI{
-    public int points;
-    public Board board;
-
-    public Player(Board board){
-        this.points = 0;
-        this.board = board;
-    }
-
-    public int getPoints(){
-        return points;
-    }
-
-    public void addPoints(int pointsToAdd){
-        this.points += pointsToAdd;
-    }
-
-    public int move(int x, int y){
-        if (x < board.getSize() && x >= 0 && y < board.getSize() && y >= 0) { // Wartości z zakresu
-            int points = board.putAndGetNewPoints(x, y);
-            if (points == 0) { // Ruch niepunktowany
-                return 0;
-            } else if (points > 0) { // Punkty dodane
-                addPoints(points);
-                return 1;
-            } else if (points < 0) {// Pole zajęte
-                return 2;
-            } else {
-                return 3;
-            }
-        }
-        else{ // Wartości spoza zakresu
-            return 4;
-        }
-
-    }
+public abstract class Player {
+    public abstract int getPoints();
+    public abstract void setBoard(Board board);
 }

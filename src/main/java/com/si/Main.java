ef8f172eaf17c345da2e1fd4cd0617cdb0452028
CrabsterK <arek.marcinowski18@gmail.com>
2018-05-20 01:16:29 +0200

    implemetn game in main

--- src/main/java/com/si/Main.java
+++ src/main/java/com/si/Main.java
@@ -1,13 +1,22 @@
 package com.si;
 
+import com.si.player.HumanPlayer;
+import com.si.player.MinMaxPlayer;
+import com.si.player.Player;
+
 public class Main {
     public static void main(String[] args) {
         final int BOARD_SIZE = 3;
+        final int DEPTH = 5;
         boolean enableGui = false;
         boolean player1MoveFirst = false;
-        
-        PersonVsPerson pvp = new PersonVsPerson(BOARD_SIZE, enableGui, player1MoveFirst);
-        pvp.runGame();
+
+        Board board = new Board(BOARD_SIZE);
+        Player player1 = new MinMaxPlayer(DEPTH);
+        Player player2 = new HumanPlayer();
+        Game game = new Game(player1, player2, BOARD_SIZE, board, enableGui, player1MoveFirst);
+        game.runGame();
+
 
 
     }

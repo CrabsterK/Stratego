0cd096c5910f718152ebba8c1ebfb510a7b69cbb
CrabsterK <arek.marcinowski18@gmail.com>
2018-05-20 01:14:18 +0200

    delete usefull classes

--- src/main/java/com/si/Game.java
+++
@@ -1,7 +0,0 @@
-package com.si;
-
-public interface Game {
-    void run();
-    void run(int[] positions);
-    void runGame();
-}
--- src/main/java/com/si/player/PlayerI.java
+++
@@ -1,7 +0,0 @@
-package com.si.player;
-
-public interface PlayerI {
-    int getPoints();
-    void addPoints(int pointsToAdd);
-    int move(int x, int y);
-}

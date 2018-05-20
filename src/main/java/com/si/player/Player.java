e48b6fe233007b17b31659179f38217c45a217ae
CrabsterK <arek.marcinowski18@gmail.com>
2018-05-20 15:21:59 +0200

    add addPoints in Player interface

--- src/main/java/com/si/player/Player.java
+++ src/main/java/com/si/player/Player.java
@@ -5,4 +5,5 @@ import com.si.Board;
 public abstract class Player {
     public abstract int getPoints();
     public abstract void setBoard(Board board);
+    public abstract void addPoints(int pointsToAdd);
 }
